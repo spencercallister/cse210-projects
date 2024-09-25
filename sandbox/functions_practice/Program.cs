46ad1bf4de56d23b33c4a5330e98675776f621d9
Spencer Callister <83098816+spencercallister@users.noreply.github.com>
2024-09-25 13:53:52 -0600

    lists and functions practice

---
+++ sandbox/functions_practice/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace functions_practice;
+
+class Program
+{
+    static void AwesomeFunction(int x)
+    {
+        Console.WriteLine($"The argument received by the awesome function is {x}.");
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        AwesomeFunction(188999);
+    }
+}
---
+++ sandbox/lists_practice/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+// List<double> x;
+List<double> x = new();
+// x = [24.0533,25.4245,26.85754];
+// x = [];
+
+// x.Add(46.84);
+Console.WriteLine("Hello, World!");
+
+// gather data to fill a list
+
+double input = -1;
+while(input != 0)
+{
+    Console.Write("Enter something: ");
+    string inputString = Console.ReadLine();
+    input = double.Parse(inputString);
+    x.Add(input);
+}
+
+foreach (double number in x)
+{
+    Console.WriteLine(number);
+}
