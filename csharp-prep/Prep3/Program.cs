6000d4d71782a313b35c5073c78ce2f05ad26822
Spencer Callister <83098816+spencercallister@users.noreply.github.com>
2024-09-26 15:50:49 -0600

    Prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicNumberString = Console.ReadLine();
+        // int magicNumber = Int32.Parse(magicNumberString);
+
+        string playAgain;
+
+        do {
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,100);
+
+            string guessString;
+            int guess;
+            int guessCount = 0;
+
+            do {
+                Console.Write("What is your guess? ");
+                guessString = Console.ReadLine();
+                guess = Int32.Parse(guessString);
+
+                guessCount++;
+
+                if (guess < magicNumber) 
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber) 
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine($"You successfully guessed it on try {guessCount}.");
+                }
+
+            } while (guess != magicNumber);
+
+            Console.Write("Would you like to play again? ");
+            playAgain = Console.ReadLine().ToLower();
+
+        } while (playAgain == "yes");
+
     }
 }
