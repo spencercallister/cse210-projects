e5d10fac6e8c259c30c5bc9b0d148a8838db16a3
Spencer Callister <83098816+spencercallister@users.noreply.github.com>
2024-11-01 20:32:12 -0600

    Learning 04 Complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new("Samuel Bennett", "Multiplication");
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+
+        MathAssignment mathAssignment = new("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        string mathAssignmentSummary = mathAssignment.GetSummary();
+        string homeworkList = mathAssignment.GetHomeworkList();
+        Console.WriteLine(mathAssignmentSummary);
+        Console.WriteLine(homeworkList);
+
+        WritingAssignment writingAssignment = new("Mary Waters", "European History", "The Causes of World War II");
+        string writingAssignmentSummary = writingAssignment.GetSummary();
+        string title = writingAssignment.GetWritingInformation();
+        Console.WriteLine(writingAssignmentSummary);
+        Console.WriteLine(title);
     }
 }
