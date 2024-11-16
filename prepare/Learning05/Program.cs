ab031fbe3ef6be2c7fa5d63de6fd750ef9628edd
Spencer Callister <83098816+spencercallister@users.noreply.github.com>
2024-11-15 17:48:48 -0800

    Learning 05 Complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [];
+
+        Square square = new("red", 5);
+        shapes.Add(square);
+
+        Rectangle rectangle = new("blue", 4, 6);
+        shapes.Add(rectangle);
+
+        Circle circle = new("yellow", 3);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes) {
+            Console.WriteLine($"The {shape.GetColor()} {shape.GetType()} has an area of {shape.GetArea()}.");
+        }
     }
 }
